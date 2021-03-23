f3df0d79cfa8773d078429217e68def835bdbff9
Rohan Phanse <rohan.a.phanse@gmail.com>
2021-03-23 22:42:30 +0000

    Add comments

--- game.js
+++ game.js
@@ -41,7 +41,7 @@ class Game {
         this.keyDownListener = ["keydown", event => {
             if (event.keyCode in KEY_TO_NAME) {
                 event.preventDefault()
-                // Only add key if not already in list
+                // Only add key if not already in list of keys down
                 if (!this.keysDown.includes(KEY_TO_NAME[event.keyCode])) {
                     this.keysDown.push(KEY_TO_NAME[event.keyCode])
                     this.arrow.keysDown = this.keysDown
@@ -88,6 +88,7 @@ class Game {
             }
         }]
 
+        // Pause / play game
         this.pauseButton.addEventListener("click", () => this.pause())
         
         document.addEventListener("keyup", event => {
@@ -102,20 +103,24 @@ class Game {
             }
         })
 
+        // Start game
         this.playButton.addEventListener("click", () => {
             this.start()
             this.home.style.display = "none"
         })
 
+        // Toggle home page
         this.homeButton.addEventListener("click", () => this.homeToggle())
         this.resumeButton.addEventListener("click", () => this.homeToggle())
 
+        // Open rules page
         this.rulesButton.addEventListener("click", () => {
             this.home.style.display = "none"
             this.rules.style.display = "flex"
             this.backButton.style.display = "flex"
         })
 
+        // Hide rules page and open home page
         this.backButton.addEventListener("click", () => {
             this.home.style.display = "flex"
             this.rules.style.display = "none"
@@ -137,12 +142,15 @@ class Game {
         this.projectiles = []
         this.circle = new Circle()
 
+        // Reset data / state
         this.wave = 0
         this.paused = false
         this.gameOver = false
 
+        // Initial
         this.addListeners()
         this.enableButtons()
+        this.updateScore()
 
         this.resumeButton.style.display = "flex"
         this.veil.style.display = "none"
@@ -166,12 +174,15 @@ class Game {
             const deletedProjectiles = []
             for (let i = 0; i < this.projectiles.length; i++) {
                 if (this.projectiles[i].shouldDelete) {
+                    // Add index of projectile to be deleted to list
                     deletedProjectiles.push(i)
                 } else {
                     this.projectiles[i].draw()
                 }
             }
+            // Projectiles to be deleted
             if (deletedProjectiles.length) {
+                // Sort indexes in decreasing order
                 deletedProjectiles.sort((a, b) => b - a)
                 for (const index of deletedProjectiles) {
                     this.projectiles.splice(index, 1)
@@ -182,23 +193,28 @@ class Game {
             const deletedEnemies = []
             for (let i = 0; i < this.enemies.length; i++) {
                 if (this.enemies[i].shouldDelete) {
+                    // Add index of enemy to be deleted to list
                     deletedEnemies.push(i)
                 }
             }
+            // Enemies to be deleted
             if (deletedEnemies.length) {
+                // Sort indexes in increasing order
                 deletedEnemies.sort((a, b) => b - a)
                 for (const index of deletedEnemies) {
                     this.enemies.splice(index, 1)
                 }
             }
 
-            // Projectiles hit things
+            // Check for collision between projectile and another sprite
             for (let i = 0; i < this.projectiles.length; i++) {
+                // Projectile fired by enemy hits circle
                 if (distance(this.projectiles[i].position, this.circle.position) < 50 && this.projectiles[i].shotBy === "enemy") {
                     this.circle.isHit(this.projectiles[i].power)
                     this.projectiles[i].shouldDelete = true
                     this.projectiles[i].element.remove()
                 }
+                // Projectile fired by enemy hits arrow
                 if (distance(this.projectiles[i].position, this.arrow.position) < this.arrow.size && this.projectiles[i].shotBy === "enemy" && this.arrow.alive) {
                     this.arrow.isHit(this.projectiles[i].power)
                     this.projectiles[i].shouldDelete = true
@@ -206,6 +222,7 @@ class Game {
                 }
 
                 for (let j = 0; j < this.enemies.length; j++) {
+                    // Projectule fired by arrow hits enemy
                     if (distance(this.projectiles[i].position, this.enemies[j].position) < this.enemies[j].size && this.projectiles[i].shotBy === "arrow") {
                         this.enemies[j].isHit(this.projectiles[i].power)
                         this.projectiles[i].shouldDelete = true
@@ -219,7 +236,9 @@ class Game {
                 this.drawEnemy(this.enemies[i])
             }
 
+            // Next wave
             if (this.enemies.length === 0) {
+                // Increment number of enemies in wave by 1
                 this.wave++
                 for (let w = 0; w < this.wave; w++) {
                     this.enemies.push(new Enemy())
@@ -237,13 +256,16 @@ class Game {
     }
 
     end() {
+        // Fade out game, disable buttons
         this.gameOver = true
         this.map.style.transitionDuration = "1s"
         this.map.style.opacity = "0"
+        this.playButton.innerText = "Play"
         this.resumeButton.style.display = "none"
         this.disableButtons()
-        this.playButton.innerText = "Play"
+
         setTimeout(() => {
+            // End game, open home page, display score
             this.paused = true
             cancelAnimationFrame(this.loop)
             this.removeListeners()
@@ -259,9 +281,11 @@ class Game {
 
     pause() {
         if (this.paused) {
+            // Play
             this.paused = false
             this.loop = requestAnimationFrame(this.frame)
             this.pauseIcon.className = "fas fa-pause"
+            // Home or rules pages visible
             if (this.home.style.display === "flex" || this.rules.style.display === "flex") {
                 this.home.style.display = "none"
                 this.veil.style.display = "none"
@@ -269,6 +293,7 @@ class Game {
                 this.backButton.style.display = "none"
             } 
         } else {
+            // Pause
             this.paused = true
             cancelAnimationFrame(this.loop)
             this.pauseIcon.className = "fas fa-play"
@@ -277,14 +302,17 @@ class Game {
 
     homeToggle() {
         if (this.home.style.display === "flex") {
+            // Close home page
             this.home.style.display = "none"
             this.veil.style.display = "none"
         } else {
+            // Open home page, hide rules page
             this.home.style.display = "flex"
             this.veil.style.display = "flex"
             this.rules.style.display = "none"
             this.backButton.style.display = "none"
         }
+        // Pause game
         this.paused = false
         this.pause()
     }
@@ -310,10 +338,13 @@ class Game {
     }
 
     updateArrowPosition() {
+        // Currently, this function is not adding any functionality
+        // But it gives the option to use data from other sprites
+        // To determine the position of the arrow
         if (this.arrow.alive) {
             const pos = this.arrow.updatePosition()
             let can_update = true
-
+            
             if (can_update) {
                 this.arrow.position = pos
             }
@@ -322,13 +353,15 @@ class Game {
 
     drawEnemy(enemy) {
         if (enemy.canMove) {
+            // Enemy stops moving, can now shoot
             if (distance(enemy.position, this.circle.position) < HEIGHT / 2 + 200) {
                 enemy.canMove = false
                 enemy.canShoot = true
             }
             enemy.updatePosition()
         } else {
-            // Determine enemy target - circle or arrow
+            // Determine enemy target: circle or arrow
+            // Target is sprite that is closer
             if (this.arrow.alive) {    
                 enemy.target = distance(enemy.position, this.circle.position) < distance(enemy.position, this.arrow.position) ? this.circle : this.arrow
             } else {
@@ -359,36 +392,49 @@ class Game {
     }
 
     addListeners() {
+        // Game listeners
         document.addEventListener(...this.keyDownListener)
         document.addEventListener(...this.keyUpListener)
         this.map.addEventListener(...this.contextMenuListener)
         document.addEventListener(...this.mouseDownListener)
         document.addEventListener(...this.mouseUpListener)
+        // Arrow listeners
         this.arrow.addListeners()
     }
 
     removeListeners() {
+        // Game listeners
         document.removeEventListener(...this.keyDownListener)
         document.removeEventListener(...this.keyUpListener)
         this.map.removeEventListener(...this.contextMenuListener)
         document.removeEventListener(...this.mouseDownListener)
         document.removeEventListener(...this.mouseUpListener)
+        // Arrow listeners
         this.arrow.removeListeners()
     }
 
     disableButtons() {
+        // Make buttons unclickable, reduce opacity
         this.pauseButton.style.pointerEvents = "none"
         this.homeButton.style.pointerEvents = "none"
+        this.pauseButton.style.opacity = "0.5"
+        this.homeButton.style.opacity = "0.5"
     }
 
     enableButtons() {
+        // Make buttons clickable, full opacity
         this.pauseButton.style.pointerEvents = "all"
         this.homeButton.style.pointerEvents = "all"
+        this.pauseButton.style.opacity = "1"
+        this.homeButton.style.opacity = "1"
     }
 
     updateScore(score) {
+        // JavaScript's localStorage API used to save high score
+        // Persists even after page refreshes
         let highScore = +localStorage.getItem("high_score")
         if (isFinite(score) && isFinite(highScore) && score > highScore) {
+            // Update high score
             localStorage.setItem("high_score", score + "")
             highScore = score
         }
--- sprites.js
+++ sprites.js
@@ -27,7 +27,7 @@ class Arrow {
         this.step = 4
         this.edgeOffset = 25
         this.image = "/images/arrow.png"
-        this.shootInterval = 400
+        this.shootInterval = 200
         this.size = 40
         this.maxHealth = 100
         this.health = this.maxHealth
@@ -81,9 +81,11 @@ class Arrow {
     }
 
     updateDirection() {
+        // Change in X and Y directions between arrow sprite and mouse
         const x = this.mouse.x - (window.innerWidth / 2 + this.position.x)
         const y = this.mouse.y - (window.innerHeight / 2 - this.position.y)
 
+        // Angle
         let angle = -radToDeg(Math.atan(y / x))
         if (x < 0) {
             angle += 180
@@ -95,8 +97,11 @@ class Arrow {
     }
 
     updatePosition() {
+        // Create copy of position
         const pos = { x: this.position.x, y: this.position.y }
 
+        // Translate action to a change in position
+        // Do not move sprite if it will leave the map
         for (const action of this.keysDown) {
             switch (action) {
                 case "up":
@@ -132,6 +137,8 @@ class Arrow {
             this.updateDirection()
             this.arrow.style.left = `${WIDTH / 2 + this.position.x}px`
             this.arrow.style.top = `${HEIGHT / 2 - this.position.y}px`
+            // CSS 'transform: rotate(...);' rotates clockwise instead of standard counterclockwise
+            // Adjust by multiplying angle by -1
             this.arrowImage.style.transform = `translate(-50%, -50%) rotate(${-this.direction}deg)`
         }
     }
@@ -146,15 +153,18 @@ class Arrow {
             this.healthBar.update(this.health / this.maxHealth * 100)
 
             if (this.health === 0) {
+                // Hide arrow, reset health, change status to not alive
                 this.arrow.style.opacity = "0"
                 this.alive = false
                 this.health = this.maxHealth
                 this.healthBar.update(this.health / this.maxHealth * 100)
+                this.position.x = WIDTH / 2 - this.size
+                this.position.y = (HEIGHT / 2 - this.size) * [1, -1][Math.floor
+                (randomNumber(0, 2))]
                 setTimeout(() => {
+                    // Reveal arrow and change status to alive
                     this.alive = true
-                    this.arrow.style.opacity = "1"
-                    this.position.x = WIDTH / 2 - this.size
-                    this.position.y = (HEIGHT / 2 - this.size) * [1, -1][Math.floor(randomNumber(0, 2))]     
+                    this.arrow.style.opacity = "1"  
                 }, 4000)
             }
         }
@@ -181,7 +191,7 @@ class Projectile {
         this.image = params.image || "/images/ball.svg"
         this.size = 10
         this.power = params.power || 10
-        this.shotBy = params.shotBy || "arrow"
+        this.shotBy = params.shotBy
 
         // State
         this.shouldDelete = false
@@ -195,15 +205,18 @@ class Projectile {
     }
 
     create() {
+        // Projectile element
         const element = document.createElement("img")
         const id = generateId() 
         element.id = `projectile-${id}`
         element.className = "sprite"
         element.src = this.image
         element.draggable = false
-        this.map.append(element)
 
+        this.map.append(element)
         this.element = document.getElementById(`projectile-${id}`)
+
+        // Initial styles
         this.element.style.transform = `translate(-50%, -50%) rotate(${-this.direction}deg)`
         this.element.style.width = `${this.size}px`
     }
@@ -212,6 +225,7 @@ class Projectile {
         this.position.x += this.speed * Math.cos(degToRad(this.direction))
         this.position.y += this.speed * Math.sin(degToRad(this.direction))
 
+        // Projectile off the map
         if (
             this.position.x > WIDTH / 2 - this.edgeOffset ||
             this.position.x < -WIDTH / 2 + this.edgeOffset ||
@@ -247,7 +261,7 @@ class Circle {
         this.map = document.getElementById("map")
         
         this.create()
-        
+
         this.circle.style.left = `${WIDTH / 2 + this.position.x}px`
         this.circle.style.top = `${HEIGHT / 2 - this.position.y}px`
     }
@@ -294,7 +308,7 @@ class HealthBar {
         // Sprite
         this.sprite = sprite
 
-        // Colors
+        // Color
         this.colors = {
             green: [0, 255, 0],
             yellow: [255, 230, 0],
@@ -329,13 +343,17 @@ class HealthBar {
     }
 
     update(percent) {
+        // Use percent to determine the color value
+        // Either green, yellow, red, or any intermediate colors
+        // Calculated with vector math
+        // As RGB colors are 3D vectors
         if (percent > 50 && percent <= 100) {
             // Initial: 50 < n <= 100
             // Scaled:  1  > n >= 0
             const scale = 1 - (percent - 50) / 50
             // Subtract yellow from green vector
             const difference = vector_subtract(this.colors.yellow, this.colors.green)
-            // Add green vector to scalar product of difference and scale
+            // Add green vector to scalar product of difference vector and scale
             this.color = vector_add(this.colors.green, vector_scalar_product(difference, scale))
         } else if (percent > 10 && percent <= 50) {
             // Initial: 10 < n <= 50
@@ -343,7 +361,7 @@ class HealthBar {
             const scale = 1 - (percent - 10) / 40
             // Subtract yellow from red vector
             const difference = vector_subtract(this.colors.red, this.colors.yellow)
-            // Add yellow vector to scalar product of difference and scale
+            // Add yellow vector to scalar product of difference vector and scale
             this.color = vector_add(this.colors.yellow, vector_scalar_product(difference, scale))
         } else if (percent >= 0 && percent <= 10) {
             this.color = this.colors.red
@@ -385,6 +403,7 @@ class Enemy {
         this.enemyImage = null
         this.map = document.getElementById("map")
 
+        // Initial position
         this.position.x = -WIDTH / 2
         this.position.y = randomNumber(-HEIGHT / 2 + this.edgeOffset, HEIGHT / 2 - this.edgeOffset)
 
@@ -415,6 +434,7 @@ class Enemy {
     }
 
     updateDirection() {
+        // Change in X and Y directions from enemy to target (either arrow or circle)
         const x = this.target.position.x - this.position.x
         const y = this.target.position.y - this.position.y
 
@@ -453,6 +473,7 @@ class Enemy {
                 this.canShoot = false
                 this.shouldDelete = true
                 setTimeout(() => {
+                    // Remove element from DOM
                     this.enemy.remove()
                 }, 300)
             }
